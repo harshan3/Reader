d8d52dde79f90e01ee7ecde9c7e1f87d98d11b21
Harshaprathan Rangeswaran <harshaprathan.r@sl.aeturnum.com>
2020-09-18 07:25:49 +0530

    Add project files.

---
+++ Reader/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.cboSheet = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(89, 497);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(276, 44);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Import";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cboSheet
+            // 
+            this.cboSheet.FormattingEnabled = true;
+            this.cboSheet.Location = new System.Drawing.Point(479, 523);
+            this.cboSheet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboSheet.Name = "cboSheet";
+            this.cboSheet.Size = new System.Drawing.Size(316, 24);
+            this.cboSheet.TabIndex = 1;
+            this.cboSheet.SelectedIndexChanged += new System.EventHandler(this.cboSheet_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(403, 500);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "FIle";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1516, 444);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(479, 496);
+            this.txtFilename.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.ReadOnly = true;
+            this.txtFilename.Size = new System.Drawing.Size(457, 22);
+            this.txtFilename.TabIndex = 4;
+            this.txtFilename.TextChanged += new System.EventHandler(this.txtFilename_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(403, 526);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sheet";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(972, 497);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Apply Changes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1264, 497);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(207, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 462);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1274, 25);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(803, 523);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 26);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1320, 470);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Loading";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1540, 558);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboSheet);
+            this.Controls.Add(this.btnBrowse);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ComboBox cboSheet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Reader/Form1.cs
@@ -0,0 +1,474 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataTableCollection tableCollection;
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            try
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "Excel Files|*.xls;*.xlsx;*.xlsm" })
+                {
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        txtFilename.Text = openFileDialog.FileName;
+                        using (var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                        {
+                            using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                            {
+                                DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                                {
+                                    ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                                });
+                                tableCollection = result.Tables;
+                                cboSheet.Items.Clear();
+                                foreach (DataTable table in tableCollection)
+                                    cboSheet.Items.Add(table.TableName);//add sheet to combo box
+                            }
+                        }
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("File Not Found");
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Another user is already using this file.");
+            }
+
+        }
+
+        private void txtFilename_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void cboSheet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           // this.progressBar1.Increment(1);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (txtFilename.TextLength == 0)
+            {
+                MessageBox.Show("Please select a valid file");
+
+            }
+            else if (cboSheet.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a Sheet");
+            }
+            else
+            {
+                CARD_ISSUE_TYPE();
+                CARD_HOLDER_NAME();
+                CARD_TYPE();
+                BENEFICIARY_NAME();
+                COUNTRY();
+                LIFE_INSURANCE();
+                REMARKS();
+                REMARK();
+                SEX_1();
+                PROVINCE_2();
+                BLOOD_GROUP();
+
+
+            }
+
+
+        }
+
+        //CARD ISSUE TYPE - Proper Case
+
+        private void CARD_ISSUE_TYPE()
+        {
+            TextInfo info = CultureInfo.CurrentCulture.TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'CARD ISSUE TYPE'"))
+                for (int x = 1; x < r.ItemArray.Length; x++)
+                {
+                    r[x] = info.ToTitleCase(r[x].ToString().ToLower());
+                    Console.WriteLine(r[x].ToString().ToLower());
+                }
+
+        }
+
+        //CARD HOLDER NAME - Proper Case
+
+        private void CARD_HOLDER_NAME()
+        {
+            TextInfo info = CultureInfo.CurrentCulture.TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'CARD ISSUE TYPE'"))
+                for (int x = 1; x < r.ItemArray.Length; x++)
+                {
+                    r[x] = info.ToTitleCase(r[x].ToString().ToLower());
+                    Console.WriteLine(r[x].ToString().ToLower());
+                }
+
+        }
+
+        //CARD TYPE - Proper Case
+
+        private void CARD_TYPE()
+        {
+            TextInfo info = CultureInfo.CurrentCulture.TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'CARD ISSUE TYPE'"))
+                for (int x = 1; x < r.ItemArray.Length; x++)
+                {
+                    r[x] = info.ToTitleCase(r[x].ToString().ToLower());
+                    Console.WriteLine(r[x].ToString().ToLower());
+                }
+
+        }
+
+        //BENEFICIARY NAME - Proper Case
+
+        private void BENEFICIARY_NAME()
+        {
+            TextInfo info = CultureInfo.CurrentCulture.TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'BENEFICIARY NAME'"))
+                for (int x = 1; x < r.ItemArray.Length; x++)
+                {
+                    r[x] = info.ToTitleCase(r[x].ToString().ToLower());
+                    Console.WriteLine(r[x].ToString().ToLower());
+                }
+
+        }
+
+        //COUNTRY - Proper Case
+
+        private void COUNTRY()
+        {
+            TextInfo info = CultureInfo.CurrentCulture.TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'COUNTRY'"))
+                for (int x = 1; x < r.ItemArray.Length; x++)
+                {
+                    r[x] = info.ToTitleCase(r[x].ToString().ToLower());
+                    Console.WriteLine(r[x].ToString().ToLower());
+                }
+
+        }
+
+
+
+
+
+        //LIFE INSURANCE UPPER CASE
+
+        private void LIFE_INSURANCE()
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'LIFE INSURANCE'"))
+                for (int y = 1; y < r.ItemArray.Length; y++)
+                {
+                    r[y] = textInfo.ToUpper(r[y].ToString());
+                    Console.WriteLine(r[y].ToString()); //WAR AND PEACE}
+                }
+        }
+
+        //REMARKS - UPPER CASE
+
+        private void REMARKS()
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'REMARKS'"))
+                for (int y = 1; y < r.ItemArray.Length; y++)
+                {
+                    r[y] = textInfo.ToUpper(r[y].ToString());
+                    Console.WriteLine(r[y].ToString()); //WAR AND PEACE}
+                }
+        }
+
+        private void REMARK()
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'REMARK'"))
+                for (int y = 1; y < r.ItemArray.Length; y++)
+                {
+                    r[y] = textInfo.ToUpper(r[y].ToString());
+                    Console.WriteLine(r[y].ToString()); //WAR AND PEACE}
+                }
+        }
+
+        //SEX 1 - UPPER CASE
+
+        private void SEX_1()
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'SEX 1'"))
+                for (int y = 1; y < r.ItemArray.Length; y++)
+                {
+                    r[y] = textInfo.ToUpper(r[y].ToString());
+                    Console.WriteLine(r[y].ToString()); //WAR AND PEACE}
+                }
+        }
+
+        //PROVINCE 2 - UPPER CASE
+
+        private void PROVINCE_2()
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'PROVINCE 2'"))
+                for (int y = 1; y < r.ItemArray.Length; y++)
+                {
+                    r[y] = textInfo.ToUpper(r[y].ToString());
+                    Console.WriteLine(r[y].ToString()); //WAR AND PEACE}
+                }
+        }
+
+
+        //BLOOD GROUP - UPPER CASE
+
+        private void BLOOD_GROUP()
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            DataTable dt = dataGridView1.DataSource as DataTable;
+
+            foreach (DataRow r in dt.Select("[SET 1] = 'BLOOD GROUP'"))
+                for (int y = 1; y < r.ItemArray.Length; y++)
+                {
+                    r[y] = textInfo.ToUpper(r[y].ToString());
+                    Console.WriteLine(r[y].ToString()); //WAR AND PEACE}
+                }
+        }
+
+
+
+
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (!backgroundWorker1.CancellationPending)
+            {
+
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label3.Text = String.Format("Loading...{0}",e.ProgressPercentage);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if(e.Error == null)
+            {
+                Thread.Sleep(100);
+                label3.Text = "Data is loaded sucessfully";
+                progressBar1.Update();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtFilename.TextLength == 0)
+            {
+                MessageBox.Show("Please select a valid file");
+
+            }
+            else if (cboSheet.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a Sheet");
+            }
+            else if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("Please open the Sheet");
+
+            }
+            else
+            {
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Excel Documents (*.xls)|*.xls";
+                sfd.FileName = "Inventory_Adjustment_Export.xls";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    // Copy DataGridView results to clipboard
+                    copyAlltoClipboard();
+
+                    object misValue = System.Reflection.Missing.Value;
+                    Excel.Application xlexcel = new Excel.Application();
+
+                    xlexcel.DisplayAlerts = false; // Without this you will get two confirm overwrite prompts
+                    Excel.Workbook xlWorkBook = xlexcel.Workbooks.Add(misValue);
+                    Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                    // Format column D as text before pasting results, this was required for my data
+                    Excel.Range rng = xlWorkSheet.get_Range("D:D").Cells;
+                    rng.NumberFormat = "@";
+
+                    // Paste clipboard results to worksheet range
+                    Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[1, 1];
+                    CR.Select();
+                    xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+
+                    // For some reason column A is always blank in the worksheet. ¯\_(ツ)_/¯
+                    // Delete blank column A and select cell A1
+                    Excel.Range delRng = xlWorkSheet.get_Range("A:A").Cells;
+                    delRng.Delete(Type.Missing);
+                    xlWorkSheet.get_Range("A1").Select();
+                    xlWorkSheet.Columns.AutoFit();
+
+                    // rng.Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red);
+                    /*
+                                    var columnHeadingsRange = xlWorkSheet.Range[
+                                        xlWorkSheet.Cells["1", "A"],
+                                        xlWorkSheet.Cells["1", "Z"]];           
+                                    columnHeadingsRange.Interior.Color = Excel.XlRgbColor.rgbLightGoldenrodYellow;
+                                    columnHeadingsRange.Style.Font.Bold = true;
+                    */
+
+
+
+                    // Save the excel file under the captured location from the SaveFileDialog
+                    xlWorkBook.SaveAs(sfd.FileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                    xlexcel.DisplayAlerts = true;
+                    xlWorkBook.Close(true, misValue, misValue);
+                    xlexcel.Quit();
+
+                    releaseObject(xlWorkSheet);
+                    releaseObject(xlWorkBook);
+                    releaseObject(xlexcel);
+
+                    // Clear Clipboard and DataGridView selection
+                    Clipboard.Clear();
+                    dataGridView1.ClearSelection();
+
+                    // Open the newly saved excel file
+                    if (File.Exists(sfd.FileName))
+                        System.Diagnostics.Process.Start(sfd.FileName);
+                }
+
+            }
+
+
+
+        }
+
+        private void copyAlltoClipboard()
+        {
+            dataGridView1.SelectAll();
+            DataObject dataObj = dataGridView1.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occurred while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+
+        void DisplayTable()
+        {
+            DataTable dt = tableCollection[cboSheet.SelectedItem.ToString()];
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (cboSheet.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a Sheet");
+
+            }
+            else
+            {
+                /* using (Form2 frm = new Form2(DisplayTable))
+                 {
+                     frm.ShowDialog(this);
+
+                 }
+                */
+             
+                DisplayTable();
+               
+            }
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
